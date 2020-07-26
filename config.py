c027024d954930ac32f952cffc9777824b730d9b
robertbrada <bert.blader@gmail.com>
2020-07-26 14:30:09 +0200

    Loading new YouTube videos

--- config.py
+++ config.py
@@ -18,3 +18,9 @@ class Config:
         self.REPRESENTATIONS = f'{self.PROJECT_ROOT}/representations.pickle'
 
         self.VIDEO_DIR = f'{self.PROJECT_ROOT}/video_out'
+
+        # YouTube related
+        self.YOUTUBE_API_KEY_PATH = f'{self.PROJECT_ROOT}/api_key'
+        self.PROCESSED_YOUTUBE_IDS = f'{self.PROJECT_ROOT}/processed_ids.pickle'
+        # The time between new video checks [in seconds]
+        self.SLEEP_INTERVAL = 600
--- main.py
+++ main.py
@@ -1,22 +1,47 @@
 from argparse import ArgumentParser
+from os.path import exists
+from time import sleep
 
+import requests
 from moviepy.editor import *
 from pafy import pafy
 
 from classifierwrapper import ClassifierWrapper
 from config import Config
-from pickle_utils import load_pickle
+from pickle_utils import load_pickle, save_pickle
 from videoprocessor import VideoProcessor
+
+
+def get_next_new_channel_vid(channel_id, api_key, processed_ids_path):
+    processed_ids = set()
+    if exists(processed_ids_path):
+        processed_ids = load_pickle(processed_ids_path)
+
+    params = {
+        'channelId': channel_id,
+        'key': api_key,
+        'part': 'snippet',
+        'type': 'video'
+    }
+    videos = requests.get(url='https://www.googleapis.com/youtube/v3/search', params=params).json()
+    for video_dict in videos['items']:
+        video_id, title = video_dict['id']['videoId'], video_dict['snippet']['title']
+        if video_id not in processed_ids:
+            processed_ids.add(video_id)
+            yield video_id, title
+
+    save_pickle(processed_ids_path, processed_ids)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser(description='A bot saving video clips from YouTube video stream'
                                         'containing people whose faces are in the dataset.')
-    parser.add_argument('--url', help='URL of the video stream.', required=True, type=str)
+    parser.add_argument('--channel-id', help='URL of the video stream.', required=True, type=str)
     parser.add_argument("--display-video", default=False, action="store_true",
                         help="Pass this flag as argument to display the video while processing.")
     args = parser.parse_args()
 
-    # Load configuration and model
+    # Load configuration
     conf = Config()
 
     # Load reference features and labels.
@@ -24,11 +49,21 @@ if __name__ == '__main__':
     classifier_wrapper = ClassifierWrapper(conf.MODEL_PATH, ref_labels, ref_features)
     video_processor = VideoProcessor(ref_labels, ref_features, classifier_wrapper, conf.VIDEO_DIR, args.display_video)
 
-    # Get the video stream and pass it to OpenCV
-    # video = pafy.new(args.url)
-    video = pafy.new('https://www.youtube.com/watch?v=K_IR90FthXQ')
-    stream = video.getbest(preftype='mp4')
+    # Load the YouTube API key
+    with open(conf.YOUTUBE_API_KEY_PATH) as f:
+        api_key = f.read()
 
-    video = VideoFileClip(stream.url)
+    while True:
+        try:
+            for video_id, video_title in get_next_new_channel_vid(args.channel_id, api_key, conf.PROCESSED_YOUTUBE_IDS):
+                video = pafy.new(f'https://www.youtube.com/watch?v={video_id}')
+                stream = video.getbest(preftype='mp4')
+                video = VideoFileClip(stream.url)
 
-    video_processor.process(video, conf.NTH_FRAME, conf.RECORD_IF_IN_M_ANAL)
+                print(f'Processing video with title: {video_title}')
+                video_processor.process(video, conf.NTH_FRAME, conf.RECORD_IF_IN_M_ANAL)
+
+            print(f'Going to sleep for {conf.SLEEP_INTERVAL} seconds')
+            sleep(conf.SLEEP_INTERVAL)
+        except KeyboardInterrupt:
+            print('Exiting')
